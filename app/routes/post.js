b63393c982e112c330694517d5bc845c0b131327
pxai <pello_altadill@cuatrovientos.org>
2017-08-25 12:58:23 +0200

    Params passed to route

--- app/routes/post.js
+++ app/routes/post.js
@@ -1,7 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-model() {
-    return this.store.findRecord('post', 2);
+model(params) {
+    return this.store.findRecord('post', params.postid);
   }
 });
